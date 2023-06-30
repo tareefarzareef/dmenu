6ade20da8d05c9c2158fc541b80517847c8d36cc
tareefarzareef <thetareefarz@gmail.com>
2023-06-30 23:15:07 +0600

    bozonic commit

--- config.h
+++ config.h
@@ -6,10 +6,9 @@ static int centered = 1;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Cascadia Code:size=10",
+	"CaskaydiaCove Nerd Font:size=10",
 	"JoyPixels:size=10",
 	"NotoColorEmoji:size=10",
-	"CaskaydiaCove Nerd Font:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
